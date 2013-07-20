73ca7a586e00b991e5422e4a7cc74aa578e650a0
Eric Hu <eric@lemurheavy.com>
2013-07-20 16:43:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chit::Application.config.secret_key_base = '3d84e1ac3ee1afaa7a76e8e23f7805ed133f2b15f34fce1d69e4944cde16d97974c358c16b675b82841363597a8cdb0d8cd85e5523346040edc4b846d822e1b7'
